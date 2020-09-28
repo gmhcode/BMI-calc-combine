61709bd28b184a3c57ba8cff9556ac242e8dc951
Greg Hughes <dcgg_101@hotmail.com>
2020-09-28 17:36:12 -0600

    Initial Commit

---
+++ BMI-calc-combine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI-calc-combine
+//
+//  Created by Greg Hughes on 9/28/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
